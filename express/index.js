c5a3ad17347a5d8a8ad767261dc297336e9776a7
z-memory <dont@forgetmy.email>
2022-02-14 20:55:46 +0100

    add console logging middleware

--- express/index.js
+++ express/index.js
@@ -2,25 +2,31 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use((req, res, next) => {
+  // middleware
+  console.log(req.method, req.path);
+  next();
+}); // console.log the request before doing anything else
+
 app.use(express.static("public")); // serve static files from the /public folder
 
 app.route("/download").get((req, res) => {
   console.log(
     "date: " + new Date().toDateString() + " route: /download method: get "
   );
-  const file = `${__dirname}/public/index.html`;
-
+  const file = `${__dirname}/public/main.css`;
+  console.log(__dirname);
   res.download(file);
 });
 
 app
   .route("/")
-  //   .get((req, res) => {
-  //     console.log(
-  //       "date: " + new Date().toDateString() + " route: / method: get "
-  //     );
-  //     res.send("We retrieve a resource ");
-  //   })
+  .get((req, res) => {
+    console.log(
+      "date: " + new Date().toDateString() + " route: / method: get "
+    );
+    res.sendFile(`${__dirname}/public/qwdqwdqwdqwd.html`);
+  })
   .post((req, res) => {
     console.log(
       "date: " + new Date().toDateString() + " route: / method: post "
